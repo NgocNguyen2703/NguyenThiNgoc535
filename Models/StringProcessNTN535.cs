0d136015628c70539afe7a4be1efc4ae6519b638
NgocNguyen2703 <nguyenthingoc270300@gmail.com>
2021-12-15 13:28:16 +0700

    Final_Commit_NTN535

---
+++ Models/StringProcessNTN535.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenThiNgoc535.Models
+{
+    public class StringProcessNTN535
+    {
+        public String LowerToUpper(string name) {
+            return name.ToUpper();
+        }
+    }
+}
